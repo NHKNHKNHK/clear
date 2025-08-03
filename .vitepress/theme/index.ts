6402495e40889c25472476f8a6a915452b1db247
ninghongkang <13605975424@163.com>
2025-08-03 22:22:35 +0800

    feat: vitepress切换路由进度条

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -6,13 +6,16 @@ import MyLayout from './MyLayout.vue'
 // import Confetti from './components/Confetti.vue'
 import { onMounted, watch, nextTick } from "vue"
 import { useRoute } from "vitepress"
-import mediumZoom from "medium-zoom"
+import mediumZoom from "medium-zoom" // 图片放大插件
+import { inBrowser } from "vitepress";
+import { NProgress } from "nprogress-v2/dist/index.js"; // 进度条组件
 
 import './styles/style.css'
 import './styles/index.scss'
 import './styles/resetMediumZoom.css'
 
 import 'virtual:group-icons.css' //代码组样式
+import "nprogress-v2/dist/index.css"; // 进度条样式
 
 // 彩虹背景动画样式
 let homePageStyle: HTMLStyleElement | undefined
@@ -44,8 +47,19 @@ export default {
       )
     }
 
+    // 切换路由进度条
+    if (inBrowser) {
+      NProgress.configure({ showSpinner: false });
 
+      router.onBeforeRouteChange = () => {
+        NProgress.start(); // 开始进度条
+      };
+      router.onAfterRouteChange = () => {
+        NProgress.done(); // 停止进度条
+      };
+    }
   },
+
 
   setup() {
     const route = useRoute()
